afa4de90f3859df15b09ee8131086b62fa4425d3
callisto-jovy <romanportius@gmail.com>
2023-03-22 17:27:39 +0100

    Fix mistake.

--- lib/src/torrent_model.dart
+++
@@ -1,4 +0,0 @@
-library torrent_model;
-
-export 'torrent_model/torrent.dart';
-export 'torrent_model/torrent_file.dart';
---
+++ lib/torrent_model.dart
@@ -0,0 +1,4 @@
+library torrent_model;
+
+export 'src/torrent_model/torrent.dart';
+export 'src/torrent_model/torrent_file.dart';
